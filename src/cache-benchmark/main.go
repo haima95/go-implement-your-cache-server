e926ad1f40c263593f3e1a6ac2a779cc6bbfc064
杨象军 <yangxiangjun@yy.com>
2019-07-28 16:39:26 +0800

    no message

--- src/cache-benchmark/main.go
+++ src/cache-benchmark/main.go
@@ -142,18 +142,18 @@ func init() {
 	flag.IntVar(&total, "n", 1000, "total number of requests")
 	flag.IntVar(&valueSize, "d", 1000, "data size of SET/GET value in bytes")
 	flag.IntVar(&threads, "c", 1, "number of parallel connections")
-	flag.StringVar(&operation, "t", "set", "test set,could by get/set/mixed")
-	flag.IntVar(&keyspacelen, "r", 0, "keyspacelen,use random keys from 0 to keyspacelen-1")
+	flag.StringVar(&operation, "t", "set", "test set, could be get/set/mixed")
+	flag.IntVar(&keyspacelen, "r", 0, "keyspacelen, use random keys from 0 to keyspacelen-1")
 	flag.IntVar(&pipelen, "P", 1, "pipeline length")
 	flag.Parse()
-	fmt.Println("type is ", typ)
-	fmt.Println("server is ", server)
-	fmt.Println("total ", total, " requests")
-	fmt.Println("data size is ", valueSize)
-	fmt.Println("we have ", threads, " connections")
-	fmt.Println("operation is ", operation)
-	fmt.Println("keyspacelen is ", keyspacelen)
-	fmt.Println("pipeline length is ", pipelen)
+	fmt.Println("type is", typ)
+	fmt.Println("server is", server)
+	fmt.Println("total", total, "requests")
+	fmt.Println("data size is", valueSize)
+	fmt.Println("we have", threads, "connections")
+	fmt.Println("operation is", operation)
+	fmt.Println("keyspacelen is", keyspacelen)
+	fmt.Println("pipeline length is", pipelen)
 
 	rand.Seed(time.Now().UnixNano())
 }
@@ -184,7 +184,7 @@ func main() {
 		statTimeSum += s.time
 		fmt.Printf("%d%% requests < %d ms\n", statCountSum*100/totalCount, b+1)
 	}
-	fmt.Printf("%d usec average for each request\n", int64(statTimeSum/time.Millisecond)/int64(statCountSum))
+	fmt.Printf("%d usec average for each request\n", int64(statTimeSum/time.Microsecond)/int64(statCountSum))
 	fmt.Printf("throughput is %f MB/s\n", float64((res.getCount+res.setCount)*valueSize)/1e6/d.Seconds())
 	fmt.Printf("rps is %f\n", float64(totalCount)/float64(d.Seconds()))
 }
